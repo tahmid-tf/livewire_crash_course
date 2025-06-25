16402f42b1d2c86e1ffe4f92ccd32dd4459a9470
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2025-06-25 16:25:16 +0600

    livewire form validation implementation and learned

--- project_files/livewire-3-crash-course/app/Livewire/CreateBook.php
+++ project_files/livewire-3-crash-course/app/Livewire/CreateBook.php
@@ -2,12 +2,32 @@
 
 namespace App\Livewire;
 
+use App\Models\Book;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class CreateBook extends Component
 {
-    public function render()
+
+    public $title;
+    public $author;
+    public $rating;
+
+    protected $rules = [
+        'title' => 'required',
+        'author' => 'required',
+        'rating' => 'required|integer|between:1,10',
+    ];
+
+    public function save()
+    {
+        $validatedData = $this->validate();
+        Book::create($validatedData);
+        $this->redirect('/');
+    }
+
+    public
+    function render()
     {
         return view('livewire.create-book');
     }
--- project_files/livewire-3-crash-course/resources/views/livewire/create-book.blade.php
+++ project_files/livewire-3-crash-course/resources/views/livewire/create-book.blade.php
@@ -1,3 +1,33 @@
-<div class="create">
-    <h3>Create a new book</h3>
+<div>
+    <div class="create">
+        <h3>Create a new book</h3>
+
+        <form wire:submit="save">
+            <div class="field">
+                <label for="">Book Title</label>
+                <input type="text" wire:model="title">
+                @error('title') <span>{{ $message }}</span> @enderror
+
+            </div>
+
+            <div class="field">
+                <label for="">Book Author</label>
+                <input type="text" wire:model="author">
+                @error('author') <span>{{ $message }}</span> @enderror
+            </div>
+
+            <div class="field">
+                <label for="">Book Rating</label>
+                <input type="text" wire:model="rating">
+                @error('rating') <span>{{ $message }}</span> @enderror
+
+            </div>
+
+            <button>Add Book</button>
+        </form>
+    </div>
 </div>
+
+
+
+
